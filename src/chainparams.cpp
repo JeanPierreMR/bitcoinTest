437edb63071d00ec19805d23978ac7843a25a22e
Jean Mejicanos <jpmejicanos@ufm.edu>
2022-09-24 18:57:55 -0600

    removing seeds from bitcoin

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,15 +120,15 @@ public:
         // This is fine at runtime as we'll fall back to using them as an addrfetch if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed.cscoin.sipa.be."); // Pieter Wuille, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("dnsseed.bluematt.me."); // Matt Corallo, only supports x9
-        vSeeds.emplace_back("dnsseed.cscoin.dashjr.org."); // Luke Dashjr
-        vSeeds.emplace_back("seed.cscoinstats.com."); // Christian Decker, supports x1 - xf
-        vSeeds.emplace_back("seed.cscoin.jonasschnelli.ch."); // Jonas Schnelli, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.btc.petertodd.org."); // Peter Todd, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.cscoin.sprovoost.nl."); // Sjors Provoost
-        vSeeds.emplace_back("dnsseed.emzy.de."); // Stephan Oeste
-        vSeeds.emplace_back("seed.cscoin.wiz.biz."); // Jason Maurice
+        // vSeeds.emplace_back("seed.cscoin.sipa.be."); // Pieter Wuille, only supports x1, x5, x9, and xd
+        // vSeeds.emplace_back("dnsseed.bluematt.me."); // Matt Corallo, only supports x9
+        // vSeeds.emplace_back("dnsseed.cscoin.dashjr.org."); // Luke Dashjr
+        // vSeeds.emplace_back("seed.cscoinstats.com."); // Christian Decker, supports x1 - xf
+        // vSeeds.emplace_back("seed.cscoin.jonasschnelli.ch."); // Jonas Schnelli, only supports x1, x5, x9, and xd
+        // vSeeds.emplace_back("seed.btc.petertodd.org."); // Peter Todd, only supports x1, x5, x9, and xd
+        // vSeeds.emplace_back("seed.cscoin.sprovoost.nl."); // Sjors Provoost
+        // vSeeds.emplace_back("dnsseed.emzy.de."); // Stephan Oeste
+        // vSeeds.emplace_back("seed.cscoin.wiz.biz."); // Jason Maurice
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
@@ -233,10 +233,10 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.emplace_back("testnet-seed.cscoin.jonasschnelli.ch.");
-        vSeeds.emplace_back("seed.tbtc.petertodd.org.");
-        vSeeds.emplace_back("seed.testnet.cscoin.sprovoost.nl.");
-        vSeeds.emplace_back("testnet-seed.bluematt.me."); // Just a static list of stable node(s), only supports x9
+        // vSeeds.emplace_back("testnet-seed.cscoin.jonasschnelli.ch.");
+        // vSeeds.emplace_back("seed.tbtc.petertodd.org.");
+        // vSeeds.emplace_back("seed.testnet.cscoin.sprovoost.nl.");
+        // vSeeds.emplace_back("testnet-seed.bluematt.me."); // Just a static list of stable node(s), only supports x9
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
